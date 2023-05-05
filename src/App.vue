b15caf76cd5c8722d4d3ed031b5d5eb80cf9116c
Lyberth <119931343+Lyberth@users.noreply.github.com>
2023-05-05 11:09:56 +0200

    s
    s

--- src/App.vue
+++ src/App.vue
@@ -10,88 +10,71 @@
                 <img src="./assets/img/logo.png" class="mr-3 h-6 sm:h-9" alt="" />
                 <span class="self-center text-xl font-Segoe Print2  whitespace-nowrap dark:text-white"></span>
             </a>
-            <div class="flex items-center lg:order-2">
-                <a href="#" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Get started</a>
-                <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                    <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </button>
-            </div>
-            <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                    <li>
-                        <a href="#" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Home</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Hjem</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Portfolio</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Kompetencer</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Kontakt</a>
-                    </li>
-                </ul>
-            </div>
+            
+            
         </div>
     </nav>
 </header>
 
 
-<section class="bg-white dark:bg-gray-900">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 ">
-        <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Rasmus Lyberth Petersen</h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">En ung gut der er er født og opvokset på Grønland. Flyttede til Danmark for at studere Multimediedesigner i Esbjerg fra 2022-2024.
+<section class="bg-white dark:bg-red-900 my-20">
+    <div class="grid max-w-screen-xl px-4 py-5 mx-auto lg:gap-8 xl:gap-0 lg:py-20 lg:grid-cols-12 ">
+        <div class="mr-auto place-self-center lg:col-span-6">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-5xl dark:text-white">Rasmus Lyberth Petersen</h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-800 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">En ung gut der er er født og opvokset på Grønland. Flyttede til Danmark for at studere Multimediedesigner i Esbjerg fra 2022-2024.
               Har altid godt kunne lide at lave video og fotoredigering siden jeg var teenager og har har fået interessen af min far.  Primær interesse er inden for  Videoredigering/foto og UI/UX.</p>
             
-            <a href="https://www.dropbox.com/s/zhjm8leyo93hg5d/Web%201920%20%E2%80%93%201.pdf?dl=0" class="mr-5 inline-flex items-center justify-center px-8 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            <a href="https://www.dropbox.com/s/2eo315errob56qd/CV_Rasmus_Lyberth_Petersen%20%281%29.pdf?dl=0" class="mr-5 inline-flex items-center justify-center px-8 py-3 text-base font-medium text-center text-gray-900 border border-gray-600 rounded-lg hover:bg-gray-400 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                 Mit CV
             </a> 
-            <a href="https://www.youtube.com/watch?v=Yj2RtyC8K2U" class="inline-flex items-center justify-center px-8 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            <a href="https://www.youtube.com/watch?v=Yj2RtyC8K2U" class="inline-flex items-center justify-center px-8 py-3 text-base font-medium text-center text-gray-900 border border-gray-600 rounded-lg hover:bg-gray-400 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                 Video CV 
             </a> 
         </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+        <div class=" lg:mt-2 lg:col-span-5  my-5">
         
-            <img src="./assets/img/billedetilcv.png" alt="mockup">
+            <img src="./assets/img/billedetilcv.png" alt="">
         </div>                
     </div>
 </section>
 
-<a href="#" class=" flex flex-col items-center mx-auto bg-white border border-gray-100 rounded-lg md:max-w-xl hover:bg-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-20 ml-20 py-8">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="" alt="">
-    <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Kompetencer</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Her er nogle udsnit af nogle ting jeg har lavet. Blandt andet: UI/UX,Content Creation og Photoshop</p>
+
+
+
+
+
+<h1 class="CHROMA flex-center my-8 text-center text-4xl mx-20px py-20 ">PORTFOLIO/KOMPETENCER</h1>
+
+<h1 class="CHROMA flex-center my-8 text-center text-1xl ">Her er nogle udsnit af nogle ting jeg har lavet gennem som Multimediedesginer. Mine stærke sidder indenfor Multimediedesigner er: UI/UX,Content Creation og Photoshop</h1>
+
+
+<div class="grid gap-4 my-20">
+    
+    <div class="grid grid-cols-5 gap-15 px-20">
+        <div>
+            <img class="h-auto max-w-full rounded-lg px-10" src="./assets/img/toot2.png" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full ml-2" src="./assets/img/poster.png" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full mr-2" src="./assets/img/liverpool2.png" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full ml-4" src="./assets/img/poster2.png" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full  px-11" src="./assets/img/rooney2.png" alt="">
+        </div>
     </div>
-</a>
-
-
-
-
-<h1 class="CHROMA flex-center my-8 text-center text-4xl ">PORTFOLIO</h1>
-
-
-
-<div class="grid grid-cols-5 gap-20 align-items-center my-20">
-  <div> <img src="./assets/img/rooney2.png" alt="ml-2"></div>
-  <div> <img src="./assets/img/toot2.png" alt=""></div>
-  <div> <img src="./assets/img/poster.png" alt=""></div>
-  <div> <img src="./assets/img/liverpool2.png" alt=""></div>
-  <div> <img src="./assets/img/poster2.png" alt=""></div>
-  
-  <!-- ... -->
 </div>
 
 
 
 
-<div class="container my-24 px-6 mx-auto">
+
+
+<div class="container my-24 px-6 mx-auto py-8">
 
 <!-- Section: Design Block -->
 <section class="mb-32 text-center text-gray-800">
@@ -183,62 +166,73 @@
 
 </div>
 
-<!--Footer container-->
-<footer class="bg-neutral-100 h-auto text-center text-white dark:bg-neutral-600 ">
-  <div class="container pt-9">
-    <div class=" ml-4">
-      <a href="https://www.facebook.com/rasmus.lyberthpetersen" class="mr-9 text-neutral-800 dark:text-neutral-200">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-4 w-4"
-          fill="currentColor"
-          viewBox="0 0 24 24">
-          <path
-            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-        </svg>
-      </a>
-      <a href="https://twitter.com/RasmusLyberth" class="mr-9 text-neutral-800 dark:text-neutral-200">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-4 w-4"
-          fill="currentColor"
-          viewBox="0 0 24 24">
-          <path
-            d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-        </svg>
-      </a>
-      <a href="https://www.instagram.com/otto_dior/" class="mr-9 text-neutral-800 dark:text-neutral-200">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-4 w-4"
-          fill="currentColor"
-          viewBox="0 0 24 24">
-          <path
-            d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-        </svg>
-      </a>
-      <a href="https://www.linkedin.com/in/rasmus-lyberth-petersen-4a5265269/" class="mr-9 text-neutral-800 dark:text-neutral-200">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-4 w-4"
-          fill="currentColor"
-          viewBox="0 0 24 24">
-          <path
-            d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
-        </svg>
-      </a>
-      
+
+
+<footer class="bg-red-700">
+    <div class=" w-full max-w-screen-xl p-4 py-2 lg:py-2">
+        <div class="md:flex md:justify-between">
+          <div class="mb-2 md:mb-0">
+              
+          </div>
+          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+              <div>
+                  <h2 class="mb-6 text-sm font-semibold text-black-900 uppercase dark:text-white">CV/VIDEO</h2>
+                  <ul class="text-black-600 dark:text-black-400 font-medium">
+                      <li class="mb-4">
+                          <a href="https://www.dropbox.com/s/2eo315errob56qd/CV_Rasmus_Lyberth_Petersen%20%281%29.pdf?dl=0" class="hover:underline">CV</a>
+                      </li>
+                      <li>
+                          <a href="https://www.youtube.com/watch?v=Yj2RtyC8K2U/" class="hover:underline">VIDEO</a>
+                      </li>
+                  </ul>
+              </div>
+              <div>
+                  <h2 class="mb-6 text-sm font-semibold text-black-900 uppercase dark:text-white">Adresse</h2>
+                  <ul class="text-black-600 dark:text-black-400 font-medium">
+                      <li class="mb-4">
+                          <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Spangsbjerg Møllevej 62C</a>
+                      </li>
+                      <li>
+                          <a href="" class="hover:underline">EASV Esbjerg</a>
+                      </li>
+                  </ul>
+              </div>
+              <div>
+                  <h2 class="mb-6 text-sm font-semibold text-black-900 uppercase dark:text-white">Kontakt</h2>
+                  <ul class="text-black-600 dark:text-black-400 font-medium">
+                      <li class="mb-4">
+                          <a href="#" class="hover:underline">+45 91110348</a>
+                      </li>
+                      <li>
+                          <a href="https://www.linkedin.com/in/rasmus-lyberth-petersen-4a5265269/" class="hover:underline">LinkedIn</a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+      <hr class="my-6 border-black-200 sm:mx-auto dark:border-black-700 lg:my-8" />
+      <div class="sm:flex sm:items-center sm:justify-between">
+          <span class="text-sm text-black-500 sm:text-center dark:text-gray-400 px-30"> <a href="" class="hover:underline">Rasmus Lyberth Petersen</a>
+          </span>
+          <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+              <a href="https://www.facebook.com/rasmus.lyberthpetersen" class="text-black-500 px-5 ">
+                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
+                  <span class="sr-only">Facebook page</span>
+              </a>
+              <a href="https://www.instagram.com/otto_dior/" class="text-red-400 px-5">
+                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd" /></svg>
+                  <span class="sr-only">Instagram page</span>
+              </a>
+              <a href="https://twitter.com/RasmusLyberth" class="text-gray-500 hover:text-black-900 px-5 ">
+                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
+                  <span class="sr-only">Twitter page</span>
+              </a>
+              
+          </div>
+      </div>
     </div>
-  </div>
-  <div>
-    
-    <a
-      class="text-neutral-700 dark:text-neutral-400"
-      href="https://tailwind-elements.com/"
-      >Rasmus Lyberth Petersen</a
-    >
-  </div>
 </footer>
+
 
 
 
